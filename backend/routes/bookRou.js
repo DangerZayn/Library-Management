19c2b3ee360e340604993dc578b49fb7b4a1cd89
DangerZayn <62941645+DangerZayn@users.noreply.github.com>
2024-06-06 19:07:42 +1000

    Final

--- backend/routes/bookRou.js
+++ backend/routes/bookRou.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { bookData, featuredBooks } = require('../controllers/bookCont');
+const authMiddleware = require('../middleware/authMiddleware');
 
 router.get('/data', bookData);
-router.get('/featured', featuredBooks)
+router.get('/featured',featuredBooks)
 
 
 module.exports = router;
